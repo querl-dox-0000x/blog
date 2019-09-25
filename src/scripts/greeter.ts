c83b9f70146c4679df98e2eaa86d3b39185e4c20
Querl Dox 0000x <querl.dox.0000x@protonmail.com>
2019-09-25 16:44:04 +0000

    Update greeter.ts

--- src/scripts/greeter.ts
+++ src/scripts/greeter.ts
@@ -2,7 +2,7 @@ function greeter(person: string) {
     return "Hello, " + person;
 }
 
-//let user = "Jane User";
-let user = [0, 1, 2];
+let user = "Jane User";
+//let user = [0, 1, 2];
 
 document.body.textContent = greeter(user);
