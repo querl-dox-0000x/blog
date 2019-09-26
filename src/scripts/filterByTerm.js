eddc75a33831042e4a19f88a71cecae3bafca587
Querl Dox 0000x <querl.dox.0000x@protonmail.com>
2019-09-26 13:53:08 +0000

    Add new file

---
+++ src/scripts/filterByTerm.js
@@ -0,0 +1,10 @@
+function filterByTerm (input, searchTerm) {
+  if (!searchTerm) throw Error ("searchTerm cannot be empty");
+  if (!input.length) throw Error ("input cannot be empty");
+  const regex = new RegExp (searchTerm, "i");
+  return input.filter (function(arrayElement) {
+    return arrayElement.url.match (regex);
+  });
+}
+
+filterByTerm ("input string", "java");
